671f9c2c660df62a0c9be549ee27aa92b4e2dc25
Danielius <Danasltuu@gmail.com>
2023-03-23 21:04:55 +0200

    tik komentaras

---
+++ main.py
@@ -0,0 +1,26 @@
+# Cafeteria project : Create an live menu and payment system (a.k.a console program) :
+
+# First the program should ask if table was reserved/ not (Providing your full name) . And then if not would assign you a table
+# (there is a specific number single, double or family tables) . After table is assigned to you, system should show how many free 
+# tables are and how which are reserved/occupied. The system must be able to show name/surname of the person of the reserved table
+# (CLI option : enter reserved table nuber ; OUTCOME: Name/Surname/Time Reserved)
+# After assigning table, system should show different menu options for breakfast, lunch , dinner , drinks
+# (alcohol. alcohol free), to choose from. Special menu for vegetarian and vegan must be included too in the special menu. All menu items should have weight,
+# preparation time in minutes, calories, and price.
+# I have to have an option to change the order before the payment section. Thus I can delete, add more, update amount of the same order.
+# I should be able to choose whatever I want from all menus in one ordering. After I finish, I need to see what I chosen, the full payable amount
+# and approx waiting time for the food to be served
+# Add an option to add tips (% from the full cost) to the final bill.
+# After the payment , system should generate the receipt (logging).
+
+# pirma rasai varda patikrinam ar yra rezervaves jei yra rezervaves soriukas toks staliukas rezervuotas atsilaisvintu apie 18val
+# rezervuojam tam laikui ar pasirenkam kita stala? pasirinkus varianta tipo jau sedam i vieta (padaveja turi galimybe pasitikrinti pagal staliuko numeri ar rezervuots
+#jei rezervuotas parodo name, surname , laika)
+#duoda pasirinkti zmogui 4 variantus meniu , pusryciai , pietus , vakariene , gerimai, specialus meniu(veganams)
+#prie kiek vieno patiekalo turi buti: svori , per kiek laiko pagamina , kalorijos , kaina.
+#klientas turi tureti galimybe užsisakyti , bet jei dar nesumokejas pakeisti užsakyma , pvz prideti kitu patiekalu , pakeisti kieki , istrinti patiekala
+#kaip baigia spaudzia paziureti uzsakyma ir parodo ka susidejo i krepseli su pavadinimu kiekiu , kaina bendra suma krc ir kiek laiko reikės laukti maisto.
+# pavalgius ir norint susimokėti yra galimybe pridėti tipsui padavejui (savo nuožiura) arba 5 , 10 , 20 , 50%
+# ir pasirinkti apmokėjimo buda , jei kortele tiksli suma , jei grynais duoda pasirinkti kiek duoda 
+# kvite matosi , patiekalai kiek , kaina , bendra suma , kiek tipsu dave jei dave, bendra suma su tips , kiek dave pinigu jei grynais
+# kiek grazos turime duoti, jei kortele tai viskas zjbys kortele 50 viso 50
